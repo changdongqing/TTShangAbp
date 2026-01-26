dc5b1c93831e7c321d89e2a93a96cc9b38cfbcf3
changdongqing <changdongqing@163.com>
2026-01-26 10:49:24 +0800

    增加一个identity模块，用于扩展用户信息管理

---
+++ modules/TTShang.Identity/src/TTShang.Identity.HttpApi/IdentityController.cs
@@ -0,0 +1,12 @@
+﻿using TTShang.Identity.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace TTShang.Identity;
+
+public abstract class IdentityController : AbpControllerBase
+{
+    protected IdentityController()
+    {
+        LocalizationResource = typeof(IdentityResource);
+    }
+}
---
+++ modules/TTShang.Identity/src/TTShang.Identity.HttpApi/IdentityHttpApiModule.cs
@@ -0,0 +1,32 @@
+﻿using Localization.Resources.AbpUi;
+using TTShang.Identity.Localization;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Localization;
+using Volo.Abp.Modularity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TTShang.Identity;
+
+[DependsOn(
+    typeof(IdentityApplicationContractsModule),
+    typeof(AbpAspNetCoreMvcModule))]
+public class IdentityHttpApiModule : AbpModule
+{
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        PreConfigure<IMvcBuilder>(mvcBuilder =>
+        {
+            mvcBuilder.AddApplicationPartIfNotExists(typeof(IdentityHttpApiModule).Assembly);
+        });
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpLocalizationOptions>(options =>
+        {
+            options.Resources
+                .Get<IdentityResource>()
+                .AddBaseTypes(typeof(AbpUiResource));
+        });
+    }
+}
