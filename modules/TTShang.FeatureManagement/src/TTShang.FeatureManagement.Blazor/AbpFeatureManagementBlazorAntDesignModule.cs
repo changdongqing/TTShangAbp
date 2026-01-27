09d05955ae25431ca629dae76c87a223cd25097d
changdongqing <changdongqing@163.com>
2026-01-27 15:16:47 +0800

    TTShang.FeatureManagement.Blazor

---
+++ modules/TTShang.FeatureManagement/src/TTShang.FeatureManagement.Blazor/AbpFeatureManagementBlazorAntDesignModule.cs
@@ -0,0 +1,18 @@
+﻿using TTShang.AntDesignTheme.Blazor;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Features;
+using Volo.Abp.Mapperly;
+using Volo.Abp.Modularity;
+
+namespace TTShang.FeatureManagement.Blazor;
+
+[DependsOn(
+    typeof(AbpAspNetCoreComponentsWebAntDesignThemeModule),
+    typeof(AbpMapperlyModule),
+    typeof(AbpFeatureManagementApplicationContractsModule),
+    typeof(AbpFeaturesModule)
+)]
+public class AbpFeatureManagementBlazorAntDesignModule : AbpModule
+{
+
+}
