eb29a13bb86c6438479519a6df6563ed6da58735
changdongqing <changdongqing@163.com>
2026-01-27 15:29:39 +0800

    featuremanagement3个编译通过了

---
+++ modules/TTShang.FeatureManagement/src/TTShang.FeatureManagement.Blazor.WebAssembly/AbpFeatureManagementBlazorWebAssemblyAntDesignModule.cs
@@ -0,0 +1,15 @@
+﻿using TTShang.AntDesignTheme.Blazor.WebAssembly;
+using TTShang.FeatureManagement.Blazor;
+using Volo.Abp.FeatureManagement;
+using Volo.Abp.Modularity;
+
+namespace TTShang.FeatureManagement.Blazor.WebAssembly;
+
+[DependsOn(
+    typeof(AbpFeatureManagementBlazorAntDesignModule),
+    typeof(AbpAspNetCoreComponentsWebAssemblyAntDesignThemeModule),
+    typeof(AbpFeatureManagementHttpApiClientModule)
+)]
+public class AbpFeatureManagementBlazorWebAssemblyAntDesignModule : AbpModule
+{
+}
